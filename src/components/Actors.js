59c9ae3103e478ce9442692632ac81b4ad18090c
Kenny Yang <kennyyang19@gmail.com>
2018-09-28 22:06:42 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const actorsPage = actors.map((actor, index) => (
+    <div key={index}>
+      Name: {actor.name}
+      <p>Movies:</p>
+        <ul>
+          {actor.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+          ))}
+        </ul>
+    </div>
+  ))
+
   return (
     <div>
-      {/*{code here}*/}
+     <h1> Actors Page</h1>
+      {actorsPage}
     </div>
   );
-};
+}
 
 export default Actors;
